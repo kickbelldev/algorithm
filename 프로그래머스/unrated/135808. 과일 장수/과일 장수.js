c4c991bc6c767034d47a2f9245e6d7180fb4495a
Jongchan <kickbelldev@gmail.com>
2023-03-08 08:26:06 +0900

    [unrated] Title: 과일 장수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,14 +1,10 @@
 function solution(k, m, score) {
     score = score.sort((a, b) => b - a)
-    
-    const arr = []
-    
+    let answer = 0
     for (let i = 0; i < score.length; i++) {
         if ((i + 1) % m === 0) {
-            arr.push(score.slice(i - m + 1, i + 1))
+            answer += score[i] * m
         }
     }
-    return arr.reduce((acc, curr) => {
-        return acc + curr.sort((a, b) => a - b)[0] * m
-    }, 0)
+    return answer
 }

45164c147350dba98fdd933fbd9959ad698d7dc6
Jongchan <kickbelldev@gmail.com>
2023-03-08 09:27:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,14 @@
+function solution(A,B){
+    A = A.sort((a, b) => a - b)
+    B = B.sort((a, b) => b - a)
+    
+    const len = A.length
+    
+    let answer = 0
+    
+    for (let i = 0; i < len; i++) {
+        answer += A[i] * B[i]
+    }
+    
+    return answer
+}

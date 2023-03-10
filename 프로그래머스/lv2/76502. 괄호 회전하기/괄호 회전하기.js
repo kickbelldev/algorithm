88ca4cc2495021e4e0b26ff66ded4333b826653b
Jongchan <kickbelldev@gmail.com>
2023-03-10 23:51:43 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,4 +1,6 @@
 function solution(s) {
+    if(s.length % 2 === 1) return 0
+
     let answer = 0
     for (let x = 0; x < s.length; x++) {
         const newStr = s.slice(x) + s.slice(0, x)
@@ -10,13 +12,19 @@ function solution(s) {
     return answer
 }
 
+const mapping = { ')': '(', '}': '{', ']': '[' }
+
 function isCorrect (str) {
-    while (str.length) {
-        const newStr = str.replace(/(\(\)|\[\]|\{\})/g, '')
-        if (newStr === str) {
-            return false
+    const stack = []
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === "[" || str[i] === "(" || str[i] === "{" ) {
+            stack.push(str[i])
+        } else {
+            const last = stack.pop()
+            if(last !== mapping[str[i]]) {
+                return false
+            }
         }
-        str = newStr
     }
     return true
 }

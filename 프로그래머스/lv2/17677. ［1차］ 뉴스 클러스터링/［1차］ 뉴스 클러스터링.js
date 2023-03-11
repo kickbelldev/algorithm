60ca5bc73c438c7fbd8f21df069804b30a890798
Jongchan <kickbelldev@gmail.com>
2023-03-12 04:29:18 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.40 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,42 @@
+function solution (str1, str2) {
+    str1 = str1.toLowerCase()
+    str2 = str2.toLowerCase()
+    
+    let arr1 = []
+    let arr2 = []
+    
+    for (let i = 0; i < str1.length - 1; i++) {
+        arr1.push(str1.slice(i, i + 2))
+    }
+    
+    for (let i = 0; i < str2.length - 1; i++) {
+        arr2.push(str2.slice(i, i + 2))
+    }
+    
+    const regex = /[a-z]{2}/
+    
+    arr1 = arr1.filter((v) => regex.test(v))
+    arr2 = arr2.filter((v) => regex.test(v))
+    
+    
+    let interCnt = 0
+    
+    let unionCnt = 0
+    
+    for (let i = 0; i < arr1.length; i++) {
+        const idx = arr2.indexOf(arr1[i])
+        
+        if (~idx) {
+            interCnt++
+            arr2.splice(idx, 1)
+        }
+        
+        unionCnt++
+    }
+    
+    unionCnt += arr2.length
+    
+    console.log(interCnt, unionCnt)
+    
+    return unionCnt ? Math.floor((interCnt / unionCnt) * 65536) : 65536
+}
